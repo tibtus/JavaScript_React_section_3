75a7f7c148d8a82944e9ce3dac10822650a703fc
TibTus <tibtus@gmail.com>
2021-10-18 14:50:22 +0300

    #mod_#1_test

---
+++ script.js
@@ -0,0 +1,28 @@
+const btns = document.querySelectorAll('button'),
+    wrapper = document.querySelector('.btn-block');
+
+/* 
+btns[0].addEventListener('click', () => {
+    if (!btns[1].classList.contains('red')) {
+        btns[1].classList.add('red');
+    } else {
+        btns[1].classList.remove('red');
+    }
+
+});
+
+ */
+
+
+
+wrapper.addEventListener('click', (event) => {
+    if (event.target && event.target.tagName == "BUTTON") {
+        console.log('Hello');
+    }
+});
+
+const btn = document.createElement('button');
+btn.classList.add('red'); 
+wrapper.append(btn);
+
+
