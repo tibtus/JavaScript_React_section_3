0ee4cc8de8d4fc642160526793791eacdf9bd481
TibTus <tibtus@gmail.com>
2021-10-21 17:34:00 +0300

    #Project_Food_job_Mod_5_Modal

--- js/script.js
+++ js/script.js
@@ -110,17 +110,20 @@ window.addEventListener('DOMContentLoaded', () => {
     const modalTrigger = document.querySelectorAll('[data-modal]'),
           modal = document.querySelector('.modal'),
           modalCloseBtn = document.querySelector('[data-close]');
-          
-   
+
+            
+    function openModal() {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
+    }  
+
 
     modalTrigger.forEach(btn => {
-        btn.addEventListener('click', () => {
-               modal.classList.add('show');
-               modal.classList.remove('hide');
-               document.body.style.overflow = 'hidden';
-
-        });
+        btn.addEventListener('click', openModal);
     });
+
 
     function closeModal() {
         modal.classList.add('hide');
@@ -142,6 +145,19 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }
     });
+
+
+    const modalTimerId = setTimeout(openModal, 5000);
+
+
+    function showModalByScroll() {
+        if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
 
 
 }); 
