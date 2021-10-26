aa84da4691c2698deb6d8c22642d74306e8c7428
TibTus <tibtus@gmail.com>
2021-10-26 20:52:59 +0300

    #Project_Food_job_Mod_11_FetchAPI

--- js/script.js
+++ js/script.js
@@ -271,12 +271,7 @@ window.addEventListener('DOMContentLoaded', () => {
             
             form.insertAdjacentElement('afterend', statusMessage);
 
-
-
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-
-            request.setRequestHeader('Content-type', 'application/json');
+ 
             const formData = new FormData(form);
 
             const object = {};
@@ -284,21 +279,23 @@ window.addEventListener('DOMContentLoaded', () => {
                 object[key] = value;
             });
 
-            const json = JSON.stringify(object);
-
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if(request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    form.reset();                    
-                        statusMessage.remove();                   
-                } else {
-                    showThanksModal(message.failure);
-                }
-            });
-
+            fetch('server.php', {
+                method: "POST",
+                headers: {
+                    'Content-type' : 'application/json'
+                },
+                body: JSON.stringify(object)
+            })
+            .then(data => data.text())
+            .then(data => {
+                console.log(data);
+                showThanksModal(message.success);                                   
+                statusMessage.remove();  
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset(); 
+            })
         });
     }
 
@@ -328,5 +325,8 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 4000);
 
     }
+
+
+
 
 }); 
