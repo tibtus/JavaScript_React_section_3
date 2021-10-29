5cf33bacdbc2b19d27ebc59eb817d1a77682b653
TibTus <tibtus@gmail.com>
2021-10-29 20:38:41 +0300

    #Project_Food_job_Mod_13_db.json

--- js/script.js
+++ js/script.js
@@ -209,41 +209,32 @@ window.addEventListener('DOMContentLoaded', () => {
   
     } 
 
-    new MenuCard(
-        "img/tabs/vegy.jpg", 
-        "vegy", 
-        'Меню "Фитнес"',
-        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-        9,
-        '.menu .container',
-        'menu__item',
-        'big'
+    const getResource = async (url) => {
+        const res = await fetch(url);
 
-    ).render();
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
 
-    new MenuCard(
-        "img/tabs/elite.jpg", 
-        "elite", 
-        'Меню “Премиум”',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-        14,
-        '.menu .container',
-        'menu__item'
+        return await res.json();
+    };
 
-    ).render();
+/*     getResource('http://localhost:3000/menu')
+            .then(data => {
+                data.forEach(({img, altimg, title, descr, price}) => {
+                    new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+                });
+            }); */
 
-    new MenuCard(
-        "img/tabs/post.jpg", 
-        "post", 
-        'Меню "Постное"',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-        21,
-        '.menu .container',
-        'menu__item'
+    axios.get('http://localhost:3000/menu')   
+            .then(data => {
+                data.data.forEach(({img, altimg, title, descr, price}) => {
+                    new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+                });
+            });
+                    
 
-    ).render();
-
-
+    
     // Forms
     
     const forms = document.querySelectorAll('form');
@@ -255,10 +246,22 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     forms.forEach(item => {
-        postData(item);
+        bindpostData(item);
     });
 
-    function postData(form) {
+    const postData = async (url, data) => {
+        const res = await fetch(url, {
+            method: "POST",
+            headers: {
+                'Content-type' : 'application/json'
+            },
+            body: data
+        });
+
+        return await res.json();
+    };
+
+    function bindpostData(form) {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
 
@@ -274,19 +277,12 @@ window.addEventListener('DOMContentLoaded', () => {
  
             const formData = new FormData(form);
 
-            const object = {};
-            formData.forEach(function(value, key) {
-                object[key] = value;
-            });
+            const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
-            fetch('server.php', {
-                method: "POST",
-                headers: {
-                    'Content-type' : 'application/json'
-                },
-                body: JSON.stringify(object)
-            })
-            .then(data => data.text())
+            
+
+
+            postData('http://localhost:3000/requests', json)
             .then(data => {
                 console.log(data);
                 showThanksModal(message.success);                                   
@@ -325,11 +321,6 @@ window.addEventListener('DOMContentLoaded', () => {
         }, 4000);
 
     }
-
-
-    fetch('http://localhost:3000/menu')
-        .then(data => data.json())
-        .then(res => console.log(res));
 
 
 
